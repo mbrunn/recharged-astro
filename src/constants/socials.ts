159ab0ec5d3db0fece05324829c129704992f3b4
Michael <nixmix800@gmail.com>
2023-02-12 14:57:33 +0100

    added links for apple music and spotify

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -2,9 +2,9 @@ export const instagram_url = "https://www.instagram.com/recharged_at/"
 
 export const youtube_url = "https://www.youtube.com/@recharged3073"
 
-export const applemusic_url = ""
+export const applemusic_url = "https://music.apple.com/de/artist/recharged/1667653995"
 
-export const spotify_url = ""
+export const spotify_url = "https://open.spotify.com/artist/2q1RWrdpcKIR53XCkAdE1q?si=7zdgGLvYS7CIXHcrwqXTcA"
 
 export const facebook_url = "https://www.facebook.com/rechargedmiba/"
 
