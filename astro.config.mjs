95fd601deb1fe14266252a3de44c9e90731f03f9
Michael <nixmix800@gmail.com>
2023-02-23 19:29:38 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,40 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(),
+    NetlifyCMS({
+      config: {
+        // Use Netlify’s “Git Gateway” authentication and target our default branch
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        media_folder: "public/assets/uploads",
+        public_folder: "/assets/uploads",
+        logo_url: "/favicon.svg",
+        collections: [
+          // Define a blog post collection
+          {
+            name: 'dates',
+            label: 'Gig Dates',
+            folder: 'src/pages/dates',
+            create: true,
+            delete: true,
+            fields: [
+              { name: 'day', widget: 'date', label: 'Datum' },
+              { name: 'time', widget: 'string', label: 'Uhrzeit' },
+              { name: 'name', widget: 'string', label: 'Gig Name' },
+              { name: 'location', widget: 'string', label: 'Location (z.B.: Sommerszene)' },
+              { name: 'link', widget: 'string', label: 'Link zu Tickets / der VA' },
+              { name: 'active', widget: 'boolean', label: 'Anzeigen' },
+            ],
+          },
+        ],
+      },
+    })],
   ssr: {
     noExternal: ['@fortawesome/*']
   }
---
+++ src/pages/dates/map-day-tue-feb-21-2023-18-58-33-gmt-0100-mitteleuropäische-normalzeit-active-true-time-10-30-name-name-location-stadtsaal-link-http-www-link-at.md
@@ -0,0 +1,8 @@
+---
+day: 2023-02-21T17:58:33.276Z
+time: 10:30
+name: Name
+location: Stadtsaal
+link: http://www.link.at
+active: true
+---
