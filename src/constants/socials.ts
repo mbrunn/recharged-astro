67fe4ec19fe127a5b465623b8539bd5ff484792d
Michael <nixmix800@gmail.com>
2023-03-03 12:58:26 +0100

    changed social links to "recharged.at"

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -1,4 +1,4 @@
-export const instagram_url = "https://www.instagram.com/recharged_at/"
+export const instagram_url = "https://www.instagram.com/recharged.at/"
 
 export const youtube_url = "https://www.youtube.com/@recharged3073"
 
@@ -8,7 +8,7 @@ export const applemusic_url = "https://music.apple.com/de/artist/recharged/16676
 
 export const spotify_url = "https://open.spotify.com/artist/2q1RWrdpcKIR53XCkAdE1q?si=7zdgGLvYS7CIXHcrwqXTcA"
 
-export const facebook_url = "https://www.facebook.com/rechargedmiba/"
+export const facebook_url = "https://www.facebook.com/recharged.at/"
 
 export const email_address = "office@recharged.at"
 
