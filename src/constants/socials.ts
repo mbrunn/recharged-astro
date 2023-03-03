0be4fe01ff942ddb83b60933477e54184ffa571b
Michael <nixmix800@gmail.com>
2023-03-03 14:13:06 +0100

    added indicators to image carousel

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -1,6 +1,6 @@
 export const instagram_url = "https://www.instagram.com/recharged.at/"
 
-export const youtube_url = "https://www.youtube.com/@recharged3073"
+export const youtube_url = "https://www.youtube.com/@RECHARGED_BAND"
 
 export const youtube_video_url = "https://www.youtube-nocookie.com/embed/e8gmToIEkfI"
 
